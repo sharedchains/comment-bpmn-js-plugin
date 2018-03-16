064943c2a29a69857d992f2ad71708f03284327f
Luca Bonora <bonora.luca@gmail.com>
2018-03-16 12:22:02 +0100

    Plugin second version: open link clicking to badge
    If there's a URI, add a clickable badge overlay

--- client/client-bundle.js
+++ client/client-bundle.js
@@ -4,40 +4,11 @@
 var validUrl = require('valid-url');
 var _ = require('lodash');
 var $ = require('jquery');
-// const shell = window.require('electron').shell;
+const shell = window.require('electron').shell;
 
 var elementOverlays = [];
 
-function LinkCommentPlugin(keyboard, eventBus, elementRegistry, overlays) {
-
-  // eventBus.on('element.click', function(event) {
-  // 	var element = event.element;
-  // 	// Verifico se l'elemento è un commento ed è premuto il pulsante ctrl
-  //   if (element.type == 'bpmn:TextAnnotation' && keyboard.isCmd(event.originalEvent)) {
-  //   	// Prendo il testo del commento
-  //   	var elementDescription = element.businessObject.text;
-  //   	console.log("It's a comment, let's check if there is a link url");
-  //     var tokens = elementDescription.split(/\s+/);
-  //     var linkAlreadyOpened = false;
-  //     tokens.forEach(function(element) {
-  //       // Check if exists one _and only one_ uri in the comment
-  //       // if there is more than one uri, opens the first one
-  //       if (validUrl.isUri(element) && !linkAlreadyOpened) {
-  //         console.log('Opening link');
-  //         linkAlreadyOpened = true;
-  //         shell.openExternal(element);
-  //       }
-  //     });
-  //   }
-  // });
-
-  keyboard.addListener(function(key, modifiers) {
-    if (key === 27 && $('div.ann-val-hover.showingPopup').length !== 0) {
-      var element = $('div.ann-val-hover.showingPopup');
-      element.removeClass('showingPopup');
-      element.hide();
-    }
-  });
+function LinkCommentPlugin(eventBus, elementRegistry, overlays) {
 
   eventBus.on('shape.changed', function (event) {
     _.defer(function () {
@@ -95,14 +66,13 @@ function LinkCommentPlugin(keyboard, eventBus, elementRegistry, overlays) {
           var tokens = elementDescription.split(/\r\n|\r|\n/);
           var descriptionEscaped = '';
           var uriExists = false;
+          var uri = '';
           tokens.forEach(function(element, index) {
             // Check if exists one _and only one_ uri in the comment
             // if there is more than one uri, opens the first one
-            if (validUrl.isUri(element)) {
+            if (!uriExists && validUrl.isUri(element)) {
               uriExists = true;
-              descriptionEscaped += '<a href="' + element + '" target="_blank">'+ element + '</a>' + '<br />';
-            } else {
-              descriptionEscaped += element + '<br />';
+              uri = element;
             }
           });
           if (uriExists) {
@@ -110,20 +80,7 @@ function LinkCommentPlugin(keyboard, eventBus, elementRegistry, overlays) {
             var overlayHtml = $('<div class="ann-val-true" data-badge="D"></div>');
 
             overlayHtml.click(function(e) {
-              var badge = $(this).siblings('.ann-val-hover');
-              if (badge.length == 0){
-                $(this).after('<div class="ann-val-hover" data-badge="D">'+descriptionEscaped+'</div>');
-                badge = $(this).siblings('.ann-val-hover');
-              }
-              if ($(badge).is(":visible")){
-                $(badge).hide();
-                $(badge).removeClass('showingPopup');
-                keyboard.unbind();
-              } else {
-                $(badge).show();
-                $(badge).addClass('showingPopup');
-                keyboard.bind(document);
-              }
+              shell.openExternal(uri);
             });
 
             elementOverlays[element.id].push(
@@ -149,7 +106,6 @@ function LinkCommentPlugin(keyboard, eventBus, elementRegistry, overlays) {
     }
 
     LinkCommentPlugin.$inject = [
-      'keyboard',
       'eventBus',
       'elementRegistry',
       'overlays'
